30aac5a4ab7eb7e234db66d50dbb1502e6764718
MarcBgF05 <62720789+MarcBgF05@users.noreply.github.com>
2024-06-06 11:22:30 -0600

    :construction: Implement dependency injection for JwtService

--- server/src/user/services/user.service.ts
+++ server/src/user/services/user.service.ts
@@ -2,9 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from 'src/auth/dto';
 import { userRepository } from '../repositories/user.repository';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UserService {
+  constructor(private readonly jwt: JwtService) {}
+
   async create(createUserDto: CreateUserDto) {
     try {
       const passwordHash = await bcrypt.hash(createUserDto.password, 10);
@@ -14,7 +17,7 @@ export class UserService {
       return newUser;
     } catch (error) {
       console.log(error);
-      return new Error('');
+      return new Error('Error creating user');
     }
   }
 
