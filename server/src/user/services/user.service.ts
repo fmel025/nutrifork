435cd02182b61f7d974a220e84ad6b0ee0839755
MarcBgF05 <62720789+MarcBgF05@users.noreply.github.com>
2024-06-06 10:59:04 -0600

    :construction: Add password encryptation logic in creatreUser method

--- server/src/user/services/user.service.ts
+++ server/src/user/services/user.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from 'src/auth/dto';
 import { userRepository } from '../repositories/user.repository';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   async create(createUserDto: CreateUserDto) {
-    const userCreated = await userRepository.create(createUserDto);
-    return userCreated;
+    try {
+      const passwordHash = await bcrypt.hash(createUserDto.password, 10);
+      createUserDto.password = passwordHash;
+
+      const newUser = await userRepository.create(createUserDto);
+      return newUser;
+    } catch (error) {
+      console.log(error);
+      return new Error('');
+    }
   }
 
   findOneByEmail(email: string) {
