c8d582a1bc0d804232d05103473ee9f6712112ae
MarcBgF05 <62720789+MarcBgF05@users.noreply.github.com>
2024-06-06 15:06:59 -0600

    :sparkles: Add method to compare passwords

--- server/src/auth/services/auth.service.ts
+++ server/src/auth/services/auth.service.ts
@@ -1,12 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/services/user.service';
-
+import { LoginDto } from '../dto';
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class AuthService {
   constructor(
     private userService: UserService,
     private jwt: JwtService,
   ) {}
-  async singIn() {}
+  async singIn(user: LoginDto) {
+    const userFound = await this.userService.findOneByEmail(user.email);
+
+    if (!userFound) {
+      throw new NotFoundException('User not found');
+    }
+  }
+
+  async comparePasswords(password: string, passwordHash: string) {
+    return await bcrypt.compare(password, passwordHash);
+  }
 }
