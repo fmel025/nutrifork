985031e9327c0f5f6068aa7e5ef1b41c69cf2235
MarcBgF05 <62720789+MarcBgF05@users.noreply.github.com>
2024-06-06 11:17:28 -0600

    :wrench: Add initial JWT configurations in authentication module

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './services/auth.service';
 import { AuthController } from './controllers/auth.controller';
 import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      global: true,
+      secret: 'palabra_secreta',
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
