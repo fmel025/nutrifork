d9f9f75ccea6501eeceeff32af9d26f00dbdfd4d
MarcBgF05 <62720789+MarcBgF05@users.noreply.github.com>
2024-06-06 14:57:41 -0600

    :sparkles: Inject dependencies of UserService and JwtService in AuthService

--- server/src/auth/services/auth.service.ts
+++ server/src/auth/services/auth.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/services/user.service';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwt: JwtService,
+  ) {}
   async singIn() {}
 }
