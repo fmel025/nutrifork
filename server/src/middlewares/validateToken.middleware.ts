5a91ad7567c85f7db99fcd10d98659157d1f0672
MarcBgF05 <62720789+MarcBgF05@users.noreply.github.com>
2024-06-07 09:02:58 -0600

    :construction: Modify validateToken middleware code

--- server/src/middlewares/validateToken.middleware.ts
+++ server/src/middlewares/validateToken.middleware.ts
@@ -13,30 +13,28 @@ import { jwtConstants } from 'src/jwt.constants';
 export class AuthRequired implements NestMiddleware {
   constructor(private readonly jwt: JwtService) {}
 
-  async use(
-    @Req() req: Request,
-    @Res({ passthrough: true }) res: Response,
-    next: NextFunction,
-  ) {
+  async use(@Req() req: Request, @Res() res: Response, next: NextFunction) {
     try {
       const { access_token } = req.cookies;
-
       if (!access_token) {
-        throw new UnauthorizedException('No token provided');
+        return res.status(401).json({ message: 'No token provided' });
       }
 
-      const payload = await this.jwt.verify(access_token, {
+      const payload = await this.jwt.verifyAsync(access_token, {
         secret: jwtConstants.secret,
       });
 
       if (!payload) {
-        throw new UnauthorizedException('Unauthorized');
+        return res.status(401).json({
+          message: 'Unauthorized',
+        });
       }
 
       req.body.user = payload;
 
       next();
-    } catch (error) {
+    } catch (err) {
+      console.log('error', err);
       throw new UnauthorizedException('Invalid Token');
     }
   }
