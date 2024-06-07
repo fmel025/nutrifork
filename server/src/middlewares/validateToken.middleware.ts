446a9fde527c552f596e2f81e880b09663f058a7
MarcBgF05 <62720789+MarcBgF05@users.noreply.github.com>
2024-06-07 00:01:44 -0600

    :construction: Implement logic for token validation middleware

--- server/src/middlewares/validateToken.middleware.ts
+++ server/src/middlewares/validateToken.middleware.ts
@@ -1,9 +1,43 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import {
+  Injectable,
+  NestMiddleware,
+  Req,
+  Res,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { NextFunction, Request, Response } from 'express';
+import { jwtConstants } from 'src/jwt.constants';
 
 @Injectable()
 export class AuthRequired implements NestMiddleware {
   constructor(private readonly jwt: JwtService) {}
 
-  async use(req: any, res: any, next: (error?: Error | any) => void) {}
+  async use(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+    next: NextFunction,
+  ) {
+    try {
+      const { access_token } = req.cookies;
+
+      if (!access_token) {
+        throw new UnauthorizedException('No token provided');
+      }
+
+      const payload = await this.jwt.verify(access_token, {
+        secret: jwtConstants.secret,
+      });
+
+      if (!payload) {
+        throw new UnauthorizedException('Unauthorized');
+      }
+
+      req.body.user = payload;
+
+      next();
+    } catch (error) {
+      throw new UnauthorizedException('Invalid Token');
+    }
+  }
 }
