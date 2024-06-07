4ecfdb3d79f59761224470c98d78a102f7eb6888
MarcBgF05 <62720789+MarcBgF05@users.noreply.github.com>
2024-06-06 22:23:35 -0600

    :recycle: Refactoring the user registration controller

--- server/src/auth/controllers/auth.controller.ts
+++ server/src/auth/controllers/auth.controller.ts
@@ -29,36 +29,31 @@ export class AuthController {
     @Body() createAuthDto: CreateUserDto,
     @Res({ passthrough: true }) res: Response,
   ) {
-    try {
-      const userFound = await this.userService.findOneByEmail(
-        createAuthDto.email,
-      );
-      if (userFound) {
-        if (userFound.name === createAuthDto.name) {
-          throw new HttpException(
-            'Username and email already exists',
-            HttpStatus.BAD_REQUEST,
-          );
-        }
-        throw new HttpException('Email already exists', HttpStatus.BAD_REQUEST);
+    const userFound = await this.userService.findOneByEmail(
+      createAuthDto.email,
+    );
+    if (userFound) {
+      if (userFound.name === createAuthDto.name) {
+        throw new HttpException(
+          'Username and email already exists',
+          HttpStatus.BAD_REQUEST,
+        );
       }
-
-      const result = await this.userService.create(createAuthDto);
-
-      if (result instanceof HttpException) {
-        throw result;
-      }
-
-      const { newUser, access_token } = result;
-      res.cookie('access_token', access_token, {
-        httpOnly: true,
-        expires: new Date(Date.now() + 1 * 24 * 60 * 1000),
-      });
-      return newUser;
-    } catch (err) {
-      console.log(err);
-      throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
+      throw new HttpException('Email already exists', HttpStatus.BAD_REQUEST);
     }
+
+    const result = await this.userService.create(createAuthDto);
+
+    if (result instanceof HttpException) {
+      throw result;
+    }
+
+    const { newUser, access_token } = result;
+    res.cookie('access_token', access_token, {
+      httpOnly: true,
+      expires: new Date(Date.now() + 1 * 24 * 60 * 1000),
+    });
+    return newUser;
   }
 
   @Post('login')
