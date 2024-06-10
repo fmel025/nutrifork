203123ca45f607c477631fd8d6ef435e6f7bc165
fmel025 <00018720@uca.edu.sv>
2024-06-09 22:24:45 -0600

    📝 Add jwt secret example to dev env file

--- server/src/jwt.constants.ts
+++
@@ -1,7 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-
-export const jwtConstants = {
-  secret:
-    new ConfigService().get<string>('JWT_SECRET') ||
-    "i~Skm1<o'rF?(<0;flSr5Exl1.R,Wg|Mw2OV}6DNo8L9c}:&6C",
-};
