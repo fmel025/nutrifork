327bd0eb10373ef9e61724639513c42f1e023d5b
moralesmajo <00155020@uca.edu.sv>
2024-06-15 17:04:57 -0600

    :sparkles: make login screen responsive

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,22 +1,26 @@
 export default function Login() {
     return (
         <div className="flex justify-center items-center font-Poppins bg-accent-green bg-green bg-cover h-screen">
-            <main className="card grid grid-cols-2 rounded-md border-black border-2 shadow-[0_4px_10px_2px_rgb(0,0,0,0.25)] bg-white w-5/6 lg:w-2/3 h-fit">
-                <section className="flex flex-col justify-center pl-16 py-16">
-                    <h1 className="text-4xl text-secondary font-medium">¡Hola de nuevo!</h1>
-                    <p className="py-3 text-base text-black font-medium">Por favor, ingresa tus datos.</p>
+            <main className="card grid grid-flow-row lg:grid-cols-2 px-2 md:px-10 py-8 lg:p-0 rounded-md border-black border-2 shadow-[0_4px_10px_2px_rgb(0,0,0,0.25)] bg-white w-11/12 lg:w-2/3 h-fit">
+                <section className="flex flex-col justify-center order-last lg:order-first lg:pl-16 lg:py-16">
+                    <h1 className="text-2xl md:text-4xl text-secondary font-medium">¡Hola de nuevo!</h1>
+                    <p className="py-3 text-sm md:text-base text-black font-medium">Por favor, ingresa tus datos.</p>
+
                     <form className="w-full">
-                        <p className="py-3 text-base text-black font-normal">Usuario o correo electrónico</p>
+                        <p className="py-3 text-sm md:text-base text-black font-normal">Usuario o correo electrónico</p>
                         <input type="text" className="input h-10 rounded-md input-bordered focus:border-2 focus:outline-0 w-full" />
-                        <p className="py-3 text-base text-black font-normal">Contraseña</p>
+                        <p className="py-3 text-sm md:text-base text-black font-normal">Contraseña</p>
                         <input type="password" className="input h-10 rounded-md input-bordered focus:border-2 focus:outline-0 w-full" />
-                        <button className="btn btn-sm mt-10 text-center text-base font-normal text-white bg-dark-green hover:bg-accent-green h-10 w-72">Inicia sesión</button>
+
+                        <button className="btn btn-sm mt-10 text-center text-base font-normal text-white bg-dark-green hover:bg-accent-green h-10 w-full md:w-96 lg:w-64">Inicia sesión</button>
                         <p className="py-1 text-xs text-black font-normal">¿No tienes una cuenta? <a href="" className="text-secondary font-semibold">Regístrate</a></p>
                     </form>
+                    
                 </section>
+
                 <section className="flex flex-col items-center justify-center">
-                    <img className="w-44" src="/src/assets/nutrifork-logo-cut.webp" alt="NutriFork logo" />
-                    <img className="w-80" src="/src/assets/food-1.webp" alt="Spaghetti" />
+                    <img className="w-52 lg:w-44 p-4" src="/src/assets/nutrifork-logo-cut.webp" alt="NutriFork logo" />
+                    <img className="w-80 hidden lg:block" src="/src/assets/food-1.webp" alt="Spaghetti" />
                 </section>
             </main>
         </div>
