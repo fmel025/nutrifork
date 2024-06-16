c27b48c0255492750679854e315d9baff576c86c
fmel025 <00018720@uca.edu.sv>
2024-06-16 15:16:09 -0600

    ✨ Implement jwt module

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -4,9 +4,18 @@ import { AuthController } from './controllers/auth.controller';
 import { UserModule } from 'src/user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule, PassportModule],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '2d' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy],
 })
--- server/src/auth/services/auth.service.ts
+++ server/src/auth/services/auth.service.ts
@@ -1,12 +1,17 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from 'src/user/services/user.service';
-import { LoginDto } from '../dto';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
-  async signIn(email: string, password: string) {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async validateUser(email: string, password: string): Promise<User> {
     const userFound = await this.userService.findOneByEmail(email);
 
     if (!userFound) {
@@ -23,8 +28,18 @@ export class AuthService {
     }
 
     delete userFound.password;
-    // TODO: Return here the jwt.
+
     return userFound;
+  }
+
+  async signIn(email: string, password: string) {
+    const user = await this.validateUser(email, password);
+
+    const payload = { email: user.email, id: user.email };
+
+    return {
+      accessToken: this.jwtService.sign(payload),
+    };
   }
 
   private async comparePasswords(password: string, encryptedPassword: string) {
