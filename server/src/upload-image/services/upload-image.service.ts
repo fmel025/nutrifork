1cae068a2b935d92d410eb753128d8cb992ff00b
Fernando Melendez <00018720@uca.edu.sv>
2024-06-24 22:55:25 -0600

    ✨ Add service method to upload an image

--- server/src/upload-image/services/upload-image.service.ts
+++ server/src/upload-image/services/upload-image.service.ts
@@ -1,4 +1,36 @@
+import { CloudinaryResponse } from '@UploadImage/types';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+import { v2 as cloudinary } from 'cloudinary';
+import streamifier from 'streamifier';
 
 @Injectable()
-export class UploadImageService {}
+export class UploadImageService {
+  constructor(private readonly configService: ConfigService) {
+    cloudinary.config({
+      cloud_name: this.configService.get('cloudinaryCloudName'),
+      api_key: this.configService.get('cloudinaryApiKey'),
+      api_secret: this.configService.get('cloudinaryApiSecret'),
+    });
+  }
+
+  uploadFile(
+    file: Express.Multer.File,
+    folder: string,
+  ): Promise<CloudinaryResponse> {
+    return new Promise<CloudinaryResponse>((resolve, reject) => {
+      const uploadStream = cloudinary.uploader.upload_stream(
+        {
+          folder,
+        },
+        (error, result) => {
+          if (error) return reject(error);
+          resolve(result);
+        },
+      );
+
+      streamifier.createReadStream(file.buffer).pipe(uploadStream);
+    });
+  }
+}
