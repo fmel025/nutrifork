8469bb6e25f3a90b67bfa31fa3dff0433a06e198
Fernando Melendez <00018720@uca.edu.sv>
2024-06-24 21:59:12 -0600

    🔐 Add cloudinary secrets to env config

--- server/src/config/environment/env.config.ts
+++ server/src/config/environment/env.config.ts
@@ -2,4 +2,7 @@ export default () => ({
   port: Number(process.env.PORT || 3000),
   jwtSecret: process.env.JWT_SECRET,
   databaseUrl: process.env.DATABASE_URL,
+  cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+  cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET,
 });
