093c6fa33e2577b0af287edd3c1b90d91991a3b4
Fernando Melendez <00018720@uca.edu.sv>
2024-06-24 22:09:33 -0600

    🔧 Add upload image and user aliases to tsconfig.json

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
+import { AuthModule } from '@Auth/auth.module';
+import { UserModule } from '@User/user.module';
 import { ConfigModule } from '@Config/config.module';
+import { UploadImageModule } from '@UploadImage/upload-image.module';
 
 @Module({
-  imports: [AuthModule, UserModule, ConfigModule],
+  imports: [AuthModule, UserModule, ConfigModule, UploadImageModule],
   controllers: [],
   providers: [],
 })
