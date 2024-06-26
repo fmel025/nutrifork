e9fab787515e9f89339d6fa722bafb6166020159
Fernando Melendez <00018720@uca.edu.sv>
2024-06-25 23:15:49 -0600

    ✨ Make active variable optional at update user dto

--- server/src/user/dto/update-user.dto.ts
+++ server/src/user/dto/update-user.dto.ts
@@ -1,9 +1,10 @@
 import { CreateUserDto } from '@Auth/dto';
 import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
-import { IsBoolean, IsNotEmpty } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
   @ApiPropertyOptional()
+  @IsOptional()
   @IsNotEmpty()
   @IsBoolean()
   active?: string;
