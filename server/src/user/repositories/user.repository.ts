4f3db4f66eaf50f66241201af7348c66d8cfdfb6
Fernando Melendez <00018720@uca.edu.sv>
2024-06-25 22:20:52 -0600

    ✨ Add user repo method to update an user

--- server/src/user/repositories/user.repository.ts
+++ server/src/user/repositories/user.repository.ts
@@ -1,5 +1,8 @@
 import { prisma } from '@Common/database';
+import { User } from '@prisma/client';
 import { CreateUserDto } from 'src/auth/dto/create-user.dto';
+
+type UpdateUserType = Partial<Omit<User, 'id'>>;
 
 class UserRepository {
   async create(user: CreateUserDto) {
@@ -30,6 +33,15 @@ class UserRepository {
   async findOneByUsername(username: string) {
     const user = await prisma.user.findFirst({
       where: { username },
+    });
+
+    return user;
+  }
+
+  async update(id: string, data: UpdateUserType) {
+    const user = await prisma.user.update({
+      where: { id },
+      data,
     });
 
     return user;
