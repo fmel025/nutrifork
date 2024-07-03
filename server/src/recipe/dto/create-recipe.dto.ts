36ff721205f307d486839d6c1de4d1241a5f0167
Fernando Melendez <00018720@uca.edu.sv>
2024-07-02 22:35:19 -0600

    ✨ Add dto to create a recipe (just for admins)

--- server/src/recipe/dto/create-recipe.dto.ts
+++ server/src/recipe/dto/create-recipe.dto.ts
@@ -1,1 +1,22 @@
-export class CreateRecipeDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateRecipeDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  image: string;
+
+  @ApiProperty({ type: [String] })
+  @IsArray()
+  ingredients: string[];
+
+  @ApiProperty()
+  @IsArray()
+  categories: string[];
+}
