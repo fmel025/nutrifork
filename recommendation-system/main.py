a08ad927961d8196dc0bc0174c2c324a088456a3
Fernando Melendez <00018720@uca.edu.sv>
2024-07-07 12:53:33 -0600

    ✨ Add lifespan to populate the model on each restart

--- recommendation-system/main.py
+++ recommendation-system/main.py
@@ -1,12 +1,27 @@
-from typing import Union, List, Optional
+from typing import List
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from prisma import Prisma
+from contextlib import asynccontextmanager
+
 
 from dto.rating import Rating
 
+prisma = Prisma()
+
+@asynccontextmanager
+async def main(app: FastAPI):    
+    await prisma.connect()
+    ratings = await prisma.rating.find_many()
+    for rating in ratings:
+        print(rating.userId)
+    yield
+    await prisma.disconnect()
+
 app = FastAPI(
     title='Recomendation system for Nutrifork', 
-    description='This is the recommendation system based on colaborative filtering with ML.'
+    description='This is the recommendation system based on colaborative filtering with ML.',
+    lifespan=main
 )
 
 origins = ["*"]
@@ -25,6 +40,6 @@ def rate_recipe(rating: Rating):
     return rating
 
 @app.get('/recommendations/{userId}', response_model=List[str])
-def get_recommendations(userId: str):
+async def get_recommendations(userId: str):
     # Implement recommendation logic here
     return ['Recipe 1', 'Recipe 2', 'Recipe 3']
